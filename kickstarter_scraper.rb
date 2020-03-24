24fdf101e4a228326602d0f8f1603b69db5d5463
Anjana Nair <anju740@gmail.com>
2020-03-24 01:21:52 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -14,5 +14,6 @@ end
 # image click: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
 # location: project.css("span.location-name").text
+# funded: project.css("ul.project-stats li.first.funded strong").text
 
 create_project_hash
